0f44f90b585aa1a6c2610e9a1aa67e5fdb396110
Denis Lapuente <lapuente@abbyy.com>
2013-01-05 08:04:59 +0100

    Preferences branching.
    Contains information about the preferences stuff

--- src/com/suicune/notasupna/GeneralPreferencesFragment.java
+++ src/com/suicune/notasupna/GeneralPreferencesFragment.java
@@ -1,7 +1,20 @@
 package com.suicune.notasupna;
 
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class GeneralPreferencesFragment extends PreferenceFragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		addPreferencesFromResource(R.xml.preferences_activity);
+	}
+
+	
 
 }
