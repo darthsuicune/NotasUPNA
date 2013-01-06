df4e946af92f5768a56a0a0eafbacaf6ee0d3e98
Denis Lapuente <lapuente@abbyy.com>
2013-01-07 00:54:12 +0100

    Removed demo data. Uncommented actual connection.
    Removed the demo data from the strings files, and now the connection returns the actual connection instead of the demo data.
    ONLY FOR RELEASE

--- src/com/suicune/notasupna/helpers/ConnectLoader.java
+++ src/com/suicune/notasupna/helpers/ConnectLoader.java
@@ -1,6 +1,21 @@
 package com.suicune.notasupna.helpers;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Date;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -56,66 +71,61 @@ public class ConnectLoader extends AsyncTaskLoader<String> {
 	@Override
 	public String loadInBackground() {
 		String language = PreferencesActivity.getRecordLanguage(mContext);
-//		String response = null;
-//		URL url = null;
-//
-//		try {
-//			if (language
-//					.equalsIgnoreCase(mContext.getString(R.string.language_code_basque))) {
-//				url = new URL("https", server, port, resource_eu);
-//			} else {
-//				url = new URL("https", server, port, resource_es);
-//			}
-//		} catch (MalformedURLException e) {
-//			e.printStackTrace();
-//		}
-//
-//		/*
-//		 * Using the Apache libraries for connections allows easy handling of
-//		 * HTTP requests as this one. UsernamePasswordCredentials allows use of
-//		 * username and password for HTTP authentication both in HTTP and HTTPS.
-//		 * HttpGet allows us to make a Http GET petition to the server to
-//		 * retrieve the data using just the url we set on the PreExecute stage
-//		 * of the task.
-//		 */
-//		HttpParams httpParams = new BasicHttpParams();
-//		HttpConnectionParams.setConnectionTimeout(httpParams, 10000);
-//		HttpConnectionParams.setSoTimeout(httpParams, 15000);
-//
-//		DefaultHttpClient httpClient = new DefaultHttpClient(httpParams);
-//		CredentialsProvider credentials = httpClient.getCredentialsProvider();
-//		credentials.setCredentials(AuthScope.ANY,
-//				new UsernamePasswordCredentials(userName, passWord));
-//		httpClient.setCredentialsProvider(credentials);
-//		HttpGet request = new HttpGet(url.toExternalForm());
-//
-//		/*
-//		 * To parse the response we need to look for general Exceptions, because
-//		 * we get a lot of different ones, all meaning the same: The connection
-//		 * didn't went right. Providing we've already checked connection and
-//		 * tunnel, it must be a filtering proxy or a server error, so we will
-//		 * just cancel the task and exit with a message. The HttpResponse class
-//		 * allows us to get a full response for any well-formed request.
-//		 */
-//		try {
-//			HttpResponse httpResponse = httpClient.execute(request);
-//			InputStream input = httpResponse.getEntity().getContent();
-//			BufferedReader reader = new BufferedReader(new InputStreamReader(
-//					input));
-//			response = reader.readLine();
-//		} catch (Exception e) {
-//			response = null;
-//			e.printStackTrace();
-//		}
+		String response = null;
+		URL url = null;
+
+		try {
+			if (language
+					.equalsIgnoreCase(mContext.getString(R.string.language_code_basque))) {
+				url = new URL("https", server, port, resource_eu);
+			} else {
+				url = new URL("https", server, port, resource_es);
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * Using the Apache libraries for connections allows easy handling of
+		 * HTTP requests as this one. UsernamePasswordCredentials allows use of
+		 * username and password for HTTP authentication both in HTTP and HTTPS.
+		 * HttpGet allows us to make a Http GET petition to the server to
+		 * retrieve the data using just the url we set on the PreExecute stage
+		 * of the task.
+		 */
+		HttpParams httpParams = new BasicHttpParams();
+		HttpConnectionParams.setConnectionTimeout(httpParams, 10000);
+		HttpConnectionParams.setSoTimeout(httpParams, 15000);
+
+		DefaultHttpClient httpClient = new DefaultHttpClient(httpParams);
+		CredentialsProvider credentials = httpClient.getCredentialsProvider();
+		credentials.setCredentials(AuthScope.ANY,
+				new UsernamePasswordCredentials(mUserName, mPassWord));
+		httpClient.setCredentialsProvider(credentials);
+		HttpGet request = new HttpGet(url.toExternalForm());
+
+		/*
+		 * To parse the response we need to look for general Exceptions, because
+		 * we get a lot of different ones, all meaning the same: The connection
+		 * didn't went right. Providing we've already checked connection and
+		 * tunnel, it must be a filtering proxy or a server error, so we will
+		 * just cancel the task and exit with a message. The HttpResponse class
+		 * allows us to get a full response for any well-formed request.
+		 */
+		try {
+			HttpResponse httpResponse = httpClient.execute(request);
+			InputStream input = httpResponse.getEntity().getContent();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					input));
+			response = reader.readLine();
+		} catch (Exception e) {
+			response = null;
+			e.printStackTrace();
+		}
 		SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(mContext).edit();
 		editor.putLong(PreferencesActivity.LAST_UPDATE, new Date().getTime());
 		editor.commit();
-		if(language.equalsIgnoreCase(mContext.getString(R.string.language_code_basque))){
-			return mContext.getString(R.string.demodata_eu);
-		} else {
-			return mContext.getString(R.string.demodata);
-		}
 
-//		return response;
+		return response;
 	}
 }
